bc01f892f31a1ea8e7deeadeefd3e7d6c8eeec13
prashant143A <117192807+prashant143A@users.noreply.github.com>
2024-07-01 23:12:29 +0530

    Update m.py
    7227640591:AAE37bVDr0dD2nVnjpY4YkmrQ6mJHVfduV8
    5416041622 
    👆are updated

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('6985771977:AAHs45lBA9nZ93YsOjhr0LPxjsb6UMctdsg')
+bot = telebot.TeleBot('7227640591:AAE37bVDr0dD2nVnjpY4YkmrQ6mJHVfduV8')
 
 # Admin user IDs
-admin_id = ["6159360725"]
+admin_id = ["5416041622"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
